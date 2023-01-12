c975402c06e4e3bf850d6d54bf4f49bf9d8868e4
Ayokunle89 <adedijiayokunle6689@gmail.com>
2023-01-13 00:41:56 +0100

    Password Generator

---
+++ Day-5.py
@@ -0,0 +1,26 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print("Welcome to the PyPassword Generator!")
+password_letters = int(input("How many letters will you like in your password_list?\n"))
+password_symbols = int(input("How many symbols would you like?\n"))
+password_numbers = int(input("How many numbers would you like?\n"))
+
+password_list = []
+for alphabet in range(1, password_letters + 1):
+    password_list += random.choice(letters)
+
+for signs in range(1, password_symbols + 1):
+    password_list += random.choice(symbols)
+
+for num in range(1, password_numbers + 1):
+    password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for word in password_list:
+    password += word
+print(password)
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. Creating a tip calculator.
 3. Creating an advanture game
 4. Creating a Rock, Paper and Scissors Game.
+5. Creating a Password Generator using for loops, random and shuffle function.
